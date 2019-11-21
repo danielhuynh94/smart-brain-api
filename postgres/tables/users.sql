2bb4906cc7674a7b2bf0620a260913744b29479e
danielhuynh94 <danielhuynh94@gmail.com>
2019-11-20 22:08:45 -0500

    Added Token Authentication

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -5,7 +5,9 @@ create table users (
 	name varchar(100),
 	email text unique not null,
 	entries bigint default 0,
-	joined timestamp not null
+	joined timestamp not null,
+	age int default 0,
+	pet text
 );
 
 COMMIT;
